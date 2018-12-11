cfd1d55b1c40e26d0e6a8561bd5c28b0bf29fbc7
Khoa Huynh <khoawin92@gmail.com>
2018-12-10 17:37:38 -0800

    finished fizzbuzz on a binary tree

--- __test__/binary_trees/fizzBuzzTree.test.js
+++ __test__/binary_trees/fizzBuzzTree.test.js
@@ -13,7 +13,59 @@ describe('fizzBuzztree()', () => {
     BST.insert(2);
     BST.insert(8);
     BST.insert(11);
-
     expect(fizzBuzzTree(BST)).toBe(BST);
   });
+
+  it('should  modify values of the tree if values are divisible by 3 AND 5', () => {
+    BST.insert(30);
+    BST.insert(15);
+    BST.insert(60);
+    BST.insert(45);
+
+    let fizzBuzz = fizzBuzzTree(BST); //?
+    expect(fizzBuzz.root.val).toBe('FizzBuzz');
+    expect(fizzBuzz.root.left.val).toBe('FizzBuzz');
+    expect(fizzBuzz.root.right.val).toBe('FizzBuzz');
+  });
+
+  it('should  modify values of the tree if values are divisible by 3 only', () => {
+    BST.insert(12);
+    BST.insert(6);
+    BST.insert(3);
+    BST.insert(18);
+
+    let fizzBuzz = fizzBuzzTree(BST); //?
+    expect(fizzBuzz.root.val).toBe('Fizz');
+    expect(fizzBuzz.root.left.val).toBe('Fizz');
+    expect(fizzBuzz.root.right.val).toBe('Fizz');
+  });
+
+  it('should  modify values of the tree if values are divisible by 5 only', () => {
+    BST.insert(20);
+    BST.insert(25);
+    BST.insert(10);
+    BST.insert(5);
+
+    let fizzBuzz = fizzBuzzTree(BST); //?
+    expect(fizzBuzz.root.val).toBe('Buzz');
+    expect(fizzBuzz.root.left.val).toBe('Buzz');
+    expect(fizzBuzz.root.right.val).toBe('Buzz');
+  });
+
+  it('should  modify/not modify values of a mixed tree', () => {
+    BST.insert(22);
+    BST.insert(30);
+    BST.insert(10);
+    BST.insert(4);
+    BST.insert(3);
+    BST.insert(23);
+
+    let fizzBuzz = fizzBuzzTree(BST); //?
+    expect(fizzBuzz.root.val).toBe(22);
+    expect(fizzBuzz.root.left.val).toBe('Buzz');
+    expect(fizzBuzz.root.right.val).toBe('FizzBuzz');
+    expect(fizzBuzz.root.left.left.val).toBe(4);
+    expect(fizzBuzz.root.left.left.left.val).toBe('Fizz');
+  });
+
 });
--- src/binary_trees/fizzBuzzTree.js
+++ src/binary_trees/fizzBuzzTree.js
@@ -1,5 +1,31 @@
 const fizzBuzzTree = (bst) => {
+  fizzBuzz(bst.root);
   return bst;
 };
+
+
+function fizzBuzz(node) {
+  node.val = changeValue(node.val);
+  if(node.right) {
+    fizzBuzz(node.right);
+  } 
+  if(node.left) {
+    fizzBuzz(node.left);
+  }
+  return node;
+}
+
+
+function changeValue(val) {
+  if(val % 3 === 0 && val % 5 === 0 ) {
+    return 'FizzBuzz';
+  } else if (val % 3 === 0) {
+    return 'Fizz';
+  } else if (val % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return val;
+  }
+}
 
 export default fizzBuzzTree;
