f684b83a051b639faa9e263e295c7a4a2aa2a99a
Khoa Huynh <khoawin92@gmail.com>
2018-12-11 16:14:05 -0800

    removed wallaby comments

--- src/string_manipulation/isPalindrome.js
+++ src/string_manipulation/isPalindrome.js
@@ -1,13 +1,13 @@
 const isPalindrome = (str) => {
   
-  let cleanStr = str.replace(/[^\dA-za-z]/g, '').toLowerCase(); //?
+  let cleanStr = str.replace(/[^\dA-za-z]/g, '').toLowerCase();
   
   if(cleanStr.length <= 1) {
     return cleanStr;
   }
   
   if(cleanStr[0] === cleanStr[cleanStr.length - 1]){
-    let newStr = cleanStr.slice(1, cleanStr.length - 1); //?
+    let newStr = cleanStr.slice(1, cleanStr.length - 1);
     return isPalindrome(newStr);
   } else {
     return false;
